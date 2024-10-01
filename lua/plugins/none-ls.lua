3b6cec2f16eacca11b8d26b2ea5402954040c2d7
Simon Koopmans <s.koopmans@climotion.com>
2024-10-01 19:10:08 +0200

    updated config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,19 +1,29 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
 
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				require("none-ls.diagnostics.eslint_d"),
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-			},
-		})
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        require("none-ls.diagnostics.eslint_d").with({
+          extra_args = function()
+            -- Check if there's a .eslintrc.yaml or .eslintrc.json in the current directory
+            local eslint_config = vim.fn.glob(".eslintrc.{json,yaml}")
+            if eslint_config ~= "" then
+              return { "--config", eslint_config }
+            end
+            return {}
+          end,
+        }),
+        null_ls.builtins.formatting.stylelint,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+  end,
 }
