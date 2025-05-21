c4b2e13ef419a01c100fa4b0a75c490346ed2cc4
Simon Koopmans <s.koopmans@climotion.com>
2025-05-21 17:47:21 +0200

    the beginning of blink?

--- lua/plugins/auto-pairs.lua
+++ lua/plugins/auto-pairs.lua
@@ -1,22 +1,22 @@
 return {
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      local npairs = require("nvim-autopairs")
-      npairs.setup({
-        disable_filetype = { "TelescopePrompt", "vim" },
-      })
-
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp_status_ok, cmp = pcall(require, "cmp")
-      if cmp_status_ok then
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-      end
-    end,
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    event = "InsertEnter",
-    config = true,
-  },
+  -- {
+  --   "windwp/nvim-autopairs",
+  --   config = function()
+  --     local npairs = require("nvim-autopairs")
+  --     npairs.setup({
+  --       disable_filetype = { "TelescopePrompt", "vim" },
+  --     })
+  --
+  --     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+  --     local cmp_status_ok, cmp = pcall(require, "cmp")
+  --     if cmp_status_ok then
+  --       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "windwp/nvim-ts-autotag",
+  --   event = "InsertEnter",
+  --   config = true,
+  -- },
 }
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,70 +1,93 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+  {
+    "saghen/blink.cmp",
+    -- optional: provides snippets for the snippet source
+    dependencies = { "rafamadriz/friendly-snippets" },
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
+    -- use a release tag to download pre-built binaries
+    version = "1.*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
 
-				-- Completions window
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = {
+        preset = "none", -- no default mappings, we define exactly what we want
 
-				-- Keybindings
-				--			mapping = cmp.mapping.preset.insert({
-				--				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				--				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				--				["<C-Space>"] = cmp.mapping.complete(),
-				--				["<C-e>"] = cmp.mapping.abort(),
-				--				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				--			}),
-				--
-				mapping = {
-					["<CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Insert,
-						select = true,
-					}),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				},
+        -- Show completion manually
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+        -- Accept with Enter, even if nothing selected
+        ["<CR>"] = { "select_and_accept", "fallback" },
+
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.is_visible() then
+              return cmp.select_next()
+            end
+          end,
+          "fallback",
+        },
+
+        -- <S-Tab>: Select previous item if visible, else fallback
+        ["<S-Tab>"] = {
+          function(cmp)
+            if cmp.is_visible() then
+              return cmp.select_prev()
+            end
+          end,
+          "fallback",
+        },
+        -- Optional movement and scroll
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        -- Signature help
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+      },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
 }
