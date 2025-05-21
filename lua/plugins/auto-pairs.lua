22b871b7a815f7462557942a024bd0716576cd31
Simon Koopmans <s.koopmans@climotion.com>
2025-05-21 17:09:05 +0200

    wonky wonky

--- lua/plugins/auto-pairs.lua
+++ lua/plugins/auto-pairs.lua
@@ -1,24 +1,22 @@
 return {
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			local npairs = require("nvim-autopairs")
-			npairs.setup({
-				disable_filetype = { "TelescopePrompt", "vim" },
-			})
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      local npairs = require("nvim-autopairs")
+      npairs.setup({
+        disable_filetype = { "TelescopePrompt", "vim" },
+      })
 
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp_status_ok, cmp = pcall(require, "cmp")
-			if cmp_status_ok then
-				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-			end
-		end,
-	},
-  	{
-		"windwp/nvim-ts-autotag",
-		ft = { "javascriptreact", "typescriptreact", "html" },
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-	},
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      if cmp_status_ok then
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = true,
+  },
 }
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -8,7 +8,6 @@ return {
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
-      autotag={ enable = true}
 		})
 	end,
 }
