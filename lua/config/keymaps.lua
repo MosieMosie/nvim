d7d7ebf915084d5310df458956cc985cbcd55fc7
Simon Koopmans <s.koopmans@climotion.com>
2025-11-26 17:12:22 +0100

    added harpoon 2

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("n", "<leader>h", ":bn <CR>")
-vim.keymap.set("n", "<leader>l", ":bp <CR>")
+-- vim.keymap.set("n", "<leader>h", ":bn <CR>")
+-- vim.keymap.set("n", "<leader>l", ":bp <CR>")
 vim.keymap.set("n", "<leader>j", ":bd <CR>")
 
 vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { silent = true })
