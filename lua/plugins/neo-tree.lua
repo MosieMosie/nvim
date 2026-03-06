f047fbeef07227e2ce77a75ddc73bf083ee0e62e
Simon Koopmans <s.koopmans@climotion.com>
2026-03-06 17:48:25 +0100

    finally fixed neo tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,5 +10,18 @@ return {
   config = function()
     vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
     vim.keymap.set("n", "<C-a>", ":Neotree focus<CR>", {})
+
+    require("neo-tree").setup({
+      filesystem = {
+        window = {
+          mappings = {
+            ["<esc>"] = "clear_filter",
+          },
+          fuzzy_finder_mappings = {
+            ["<esc>"] = "close_keep_filter",
+          },
+        },
+      },
+    })
   end,
 }
